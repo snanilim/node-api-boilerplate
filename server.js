d575593d04ff888d9defa9ff6ae32de713906f1a
snanilim <snanilim@gmail.com>
2018-07-30 16:03:17 +0600

    ts test

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
-var newName = 123;
-function hello(newName) {
-    return newName;
+var passcode = "secret passcode";
+var Employee = /** @class */ (function () {
+    function Employee() {
+    }
+    Employee.prototype.fullNames = function (name) {
+        return this._fullName;
+    };
+    return Employee;
+}());
+var employee = new Employee();
+// employee.fullNames = "Bob Smith";
+if (employee.fullNames) {
+    console.log(employee.fullNames);
 }
-var aa = hello(newName);
-console.log(aa);
--- server.ts
+++ server.ts
@@ -1,7 +1,30 @@
-var newName : number = 123;
-function hello(newName):string{
-    return newName;
+class CommonProperty{
+    header: string;
+
+    constructor(header: string){
+        this.header = header;
+    }
+    public crtColor(color:string) {
+        console.log(this.header);
+        console.log("color: "+ color);
+    }
 }
 
-var aa = hello(newName);
-console.log(aa);
+class DarkTheme extends CommonProperty{
+    constructor(header: string){
+        super(header);
+    }
+    public crtColor(color:string) {
+        super.crtColor(color);
+    }
+}
+
+class WhiteTheme extends CommonProperty{
+
+}
+
+var dark  = new DarkTheme("Dark Theme Css");
+var white = new WhiteTheme("White Theme Css");
+
+dark.crtColor("000");
+white.crtColor("fff");
