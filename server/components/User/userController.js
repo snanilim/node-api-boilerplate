6b2be3cb31850fe7c09e80fe5c653c950f7ff929
aiaoio <snanilim@gmail.com>
2018-08-09 01:08:13 +0600

    added json schema validation ajv

--- server/components/User/userController.js
+++ server/components/User/userController.js
@@ -1,6 +1,11 @@
+const Ajv = require('ajv');
 
+const schema = require('../../schema/userSchema.json');
 
 exports.signUp = (req, res, next) =>{
-    const body = req.body();
+    const body = req.body;
+    var ajv = new Ajv();
+    var valid = ajv.validate(schema, body);
+    if (!valid) console.log(ajv.errors);
 };
 
