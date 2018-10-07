ca96c62351b246f6c5b724400f7dc348a3f62014
snanilim <snanilim@gmail.com>
2018-10-07 17:29:27 +0600

    added verifyJWT and checkrole permission

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,15 +1,36 @@
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const User = require('../apps/User/userModel');
 
-const isAuthenticated = (req, res, next) => {
+const ADMIN = 'admin';
+const USER = 'user';
+
+const checkRoles = (roles, userID) => {
+    const user = User.findById(userID);
+    if (User.roles.includes(roles)) {
+        if (roles === user.role) return true;
+        return false;
+    }
+    return false;
+};
+
+const verifyJWT = async (req, res, next, roles) => {
     // const tokenValue = req.headers.authorization;
     const token = (req.headers.authorization && req.headers.authorization.split(' ')[1]) || req.cookies.token;
     try {
-        jwt.verify(token, config.get('token_secret'));
-        next();
+        const payload = jwt.verify(token, config.get('token_secret'));
+        const userID = payload.sub;
+        await checkRoles(roles, userID, next);
+        return next();
     } catch (err) {
-      throw err;
+      return next(err);
     }
 };
 
-module.exports = isAuthenticated;
+const isAuthorized = (roles = User.roles) => (req, res, next) => {
+    verifyJWT(req, res, next, roles);
+};
+
+exports.isAuthorized = isAuthorized;
+exports.ADMIN = ADMIN;
+exports.USER = USER;
